7ced7baa4b51f5665102afffa0d5e2f5ca8e94e3
largado <Kv2940bm@>
2022-09-05 22:57:39 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# data 05/09/2022
+# Alexandro Avena
+# Gera relatórios todo dia uma vez por dia
+
+import os
+from time import sleep
+
+while 1 ==1:
+      print("vou chamar o sleep")
+      sleep(86400)
+      os.system("ls")
+      print("vou finalizar o programa")
+
+
